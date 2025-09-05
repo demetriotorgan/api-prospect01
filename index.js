ed29919fa9681b1af992556dced74af5289fb037
demetriotorgan <demetriotorgan@hotmail.com>
2025-09-05 09:56:18 -0300

    loginFinal

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
 //Rotas
 const usuarioRoutes = require('./routes/userRoutes');
@@ -9,6 +10,14 @@ const usuarioRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors({
+    origin:'*',
+}));
+app.use((req,res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();    
+  });
 
 //Conecção com o banco de dados
 mongoose
