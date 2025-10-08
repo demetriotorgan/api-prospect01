abfc650c3fd72edbc9ea6eeb5d9c3eb7112e4809
demetriotorgan <demetriotorgan@hotmail.com>
2025-10-08 13:48:51 -0300

    rotaExibirEmpresaId

--- routes/estabelecimentoRoutes.js
+++ routes/estabelecimentoRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { adicionarListaEmpresas, listarEmpresas, apagarListaEmpresas, verificarEmpresas } = require('../controllers/estabelecimentoController');
+const { adicionarListaEmpresas, listarEmpresas, apagarListaEmpresas, verificarEmpresas, exibirEmpresaPorId } = require('../controllers/estabelecimentoController');
 const router = express.Router();
 
 //rota para salvar lista de empresa
@@ -7,5 +7,5 @@ router.post('/salvar-lista-empresas', adicionarListaEmpresas);
 router .get('/listar-empresas', listarEmpresas);
 router.delete('/apagar-lista-empresas', apagarListaEmpresas);
 router.post('/verificar-empresas', verificarEmpresas);
-
+router.get('/exibir-empresa/:empresaId', exibirEmpresaPorId);
 module.exports = router;
