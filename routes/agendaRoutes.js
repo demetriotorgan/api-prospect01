82f33aa888cbb2aa7f33abf274ccaa21c46c3bac
demetriotorgan <demetriotorgan@hotmail.com>
2025-10-11 12:31:05 -0300

    controllerAgendaRefatorado

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -3,10 +3,7 @@ const { getAgenda, getAgendaProximos7Dias, salvarAgendamento, excluirListaAgenda
 
 const router = express.Router();
 
-router.get('/agenda', getAgenda);
-router.get('/agenda/proximos-7-dias', getAgendaProximos7Dias);
 router.post('/salvar-agendamento', salvarAgendamento);
 router.get('/listar-agendamentos-salvos', listarAgendamentosSalvos);
 router.delete('/deletar-agendamentos', excluirListaAgendamento);
-router.delete('/excluir-agendamento/:empresaId', excluirAgendamento);
 module.exports = router;
